fe76ec40c8b4dc28004a9abed87c0f4a1e895dbb
Jerly08 <jerlyinvest@gmail.com>
2024-03-29 19:31:21 +0700

    memodifikasi file layout dan menambahkan folder dan asset-asset

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <div>
+      <h1 className="text-4xl">Evently</h1>
         <Button variant="destructive" className="px-10">Delete</Button>
-      </div>
+      
     </main>
   );
 }
