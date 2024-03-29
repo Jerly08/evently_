c713666aff8ae044242cdb1ce52ce985812fad68
Jerly08 <jerlyinvest@gmail.com>
2024-03-29 22:03:35 +0700

    menambahkan fitur login mengunakan clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({ 
   subsets: ["latin"],
@@ -11,6 +12,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Evently",
   description: "Evently is a platform for event management",
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
 };
 
 export default function RootLayout({
@@ -19,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
