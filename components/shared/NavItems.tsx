924788d494cda26f37d784ed50e7924c5aaec580
Jerly08 <jerlyinvest@gmail.com>
2024-03-30 10:16:15 +0700

    Membuat Navigasi bar

---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { headerLinks } from "@/constants"; // Import headerLinks array from a constants file
+import Link from "next/link"; // Import Link component from Next.js
+import { usePathname } from "next/navigation"; // Import usePathname hook from Next.js
+
+const NavItems = () => {
+    const pathname = usePathname(); // Get the current pathname using usePathname hook
+
+    return (
+        <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row"> {/* Flexbox container */}
+            {headerLinks.map((link) => { // Map over headerLinks array
+                const isActive = pathname === link.route; // Determine if the link is active by comparing the pathname with the link's route
+
+                return (
+                    <li
+                        key={link.route} // Set a unique key for each list item
+                        className={`flex-center p-medium-16 whitespace-nowrap ${isActive ? 'text-primary-500' : ''}`} // Add text-primary-500 class if the link is active
+                    >
+                        <Link href={link.route}>
+                            <a>{link.label}</a> {/* Render the link label */}
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default NavItems; // Export the NavItems component
