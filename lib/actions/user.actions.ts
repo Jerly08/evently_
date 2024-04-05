089aa103713efb2b057524ab65db8340f1fe4ef6
Jerly08 <jerlyinvest@gmail.com>
2024-04-05 17:18:08 +0700

    update

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,10 +2,10 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase} from '@/lib/mongodb/database'
-import User from '@/lib/mongodb/database/models/user.model'
-import Order from '@/lib/mongodb/database/models/order.model'
-import Event from '@/lib/mongodb/database/models/event.model'
+import { connectToDatabase} from '@/lib/database'
+import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
+import Event from '@/lib/database/models/event.model'
 import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
