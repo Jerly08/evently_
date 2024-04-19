f598c0542c0613a7a0e151dc1b1e39532b55cfed
Jerly08 <jerlyinvest@gmail.com>
2024-04-19 14:03:17 +0700

    Membuat Detail Page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,13 @@ import { handleError } from "../utils"
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+const populateEvent = async (query: any) => {
+    return query
+        .populate({path: 'organizer', model: User, select: '_id firstName lastName'})
+        .populate({path: 'category', model: Category, select: '_id name'})
+}
 
 export const createEvent = async ({event, userId, path}:CreateEventParams)=>{
     try {
@@ -20,5 +27,20 @@ export const createEvent = async ({event, userId, path}:CreateEventParams)=>{
     } catch (error) {
         console.log(error);
         handleError(error);
+    }
+}
+
+export const getEventById = async (eventId: string) => {
+    try {
+        await connectToDatabase();
+        const event = await populateEvent(Event.findById(eventId));
+
+        if(!event){
+            throw new Error ("Event not found")
+        }
+
+        return JSON.parse(JSON.stringify(event));
+    } catch (error) {
+        handleError(error)
     }
 }
