6af3c9fca9194aad0523c2c490534cc0630cf7bc
Jerly08 <jerlyinvest@gmail.com>
2024-04-26 15:40:47 +0700

    implement stripe

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
   getEventById,
@@ -49,7 +50,8 @@ const EventDetails = async ({
                 </p>
               </div>
             </div>
-            {/* {checkout button} */}
+
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
