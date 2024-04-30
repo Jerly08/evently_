3b0c1352cfe20b5b856788228bf69dfe5755ce06
Jerly08 <jerlyinvest@gmail.com>
2024-04-30 23:24:35 +0700

    Finishing

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -11,18 +11,20 @@ import React from "react";
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
+
   const ordersPage = Number(searchParams?.ordersPage) || 1;
   const eventsPage = Number(searchParams?.eventsPage) || 1;
+
   const orders = await getOrdersByUser({ userId, page: ordersPage });
+
   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
-  console.log({ orderedEvents });
   return (
     <>
       {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="weapper flex item-center justify-center sm:justify-between">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/#events">Explore More Events</Link>
@@ -30,29 +32,30 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
-      <section className="weapper my-8">
+      <section className="wrapper my-8">
         <Collection
           data={orderedEvents}
-          emptyTitle="No Event tickets purchased yet"
+          emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
           limit={3}
           page={ordersPage}
-          urlParamName="orderPage"
+          urlParamName="ordersPage"
           totalPages={orders?.totalPages}
         />
       </section>
 
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="weapper flex item-center justify-center sm:justify-between">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
       </section>
-      <section className="weapper my-8">
+
+      <section className="wrapper my-8">
         <Collection
           data={organizedEvents?.data}
           emptyTitle="No events have been created yet"
